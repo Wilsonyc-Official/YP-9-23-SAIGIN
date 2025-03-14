74c879dc28afcc0e6f66219236fad937526828b0
student1 <student1@class.local>
2025-03-15 08:49:37 +0900

    15.03.25

---
+++ 15.03.25/Zadanie_YP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zadanie_YP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";
+
+            switch (name)
+            {
+                case "Bob":
+                    Console.WriteLine("Ваше имя - Bob");
+                    break;
+                case "Tom":
+                    Console.WriteLine("Ваше имя - Tom");
+                    break;
+                case "Sam":
+                    Console.WriteLine("Ваше имя - Sam");
+                    break;
+                default:
+                    Console.WriteLine("Имя не найдено");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
